d344c4f2adc6d9fadf888ade1c6e53ad03f95828
Wai Yan Min <waiyann.min1001@gmail.com>
2023-05-29 02:06:09 +0700

    fix: revision

--- daily/loop/ascii/main.go
+++ daily/loop/ascii/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	for i := 33; i <= 122; i++ {
-		fmt.Printf("%v\t%#x\t%#U\n", i, i, i)
+		fmt.Printf("Value: %v\t HEXA: %#x\t Unicode Full: %#U\t Unicode Character: %c\n", i, i, i, i)
 	}
-	fmt.Println(runtime.GOARCH)
+	fmt.Println("Runtime ARCHITECTURE: ", runtime.GOARCH, "\tRuntime OS: ", runtime.GOOS)
 }
